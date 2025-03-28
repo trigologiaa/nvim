42b96e8591cbdfd30326b9826438872caa19938b
trigologiaa <augusto.ribero.15@gmail.com>
2025-03-28 12:30:09 -0300

    merge other branch

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -4,34 +4,54 @@ return {
   "epwalsh/obsidian.nvim",
   version = "*",
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
   },
   opts = {
     workspaces = {
       {
         name = "MyNotes",
-        path = '/home/gaston/on/Documents/Obsidian'
-      }
+        path = "~/Documents/Obsidian",
+      },
     },
     completion = {
-      min_chars = 2
+      min_chars = 2,
     },
     notes_subdir = "limbo",
     new_notes_location = "limbo",
     attachments = {
-      img_folder = "files"
+      img_folder = "files",
     },
     daily_notes = {
-      template = "note"
+      template = "note",
     },
     mappings = {
-      ["<leader>of"] = {action = function() return require("obsidian").util.gf_passthrough() end, opts = {noremap = false, expr = true, buffer = true}},
-      ["<leader>od"] = {action = function() return require("obsidian").util.toggle_checkbox() end, opts = {buffer = true}},
-      ["<leader>onn"] = {action = function() return require("obsidian").commands.new_note("Newsletter-Issue") end, opts = {buffer = true}},
-      ["<leader>ont"] = {action = function() return require("obsidian").util.insert_template("Newsletter-Issue") end, opts = {buffer = true}}
+      ["<leader>of"] = {
+        action = function()
+          return require("obsidian").util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      ["<leader>od"] = {
+        action = function()
+          return require("obsidian").util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      ["<leader>onn"] = {
+        action = function()
+          return require("obsidian").commands.new_note("Newsletter-Issue")
+        end,
+        opts = { buffer = true },
+      },
+      ["<leader>ont"] = {
+        action = function()
+          return require("obsidian").util.insert_template("Newsletter-Issue")
+        end,
+        opts = { buffer = true },
+      },
     },
     note_frontmatter_func = function(note)
-      local out = {id = note.id, aliases = note.aliases, tags = note.tags}
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
       if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
         for k, v in pairs(note.metadata) do
           out[k] = v
@@ -54,7 +74,7 @@ return {
       subdir = "templates",
       date_format = "%Y-%m-%d-%a",
       gtime_format = "%H:%M",
-      tags = ""
-    }
-  }
+      tags = "",
+    },
+  },
 }
