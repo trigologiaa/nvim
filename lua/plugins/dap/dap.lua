b41021223f32abe20d8f6a88a8c5b431ef4af711
trigologiaa <augusto.ribero.15@gmail.com>
2026-01-14 15:37:14 -0300

    chore: codelldb dap configuration for C

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -17,6 +17,27 @@ return {
 		require("dap-python").test_runner = "pytest"
 		local mason_pkg_path = vim.fn.expand("~/.local/share/nvim/mason/packages/local-lua-debugger-vscode/extension/")
 		local adapter_path = mason_pkg_path .. "extension/debugAdapter.js"
+		dap.adapters.codelldb = {
+			type = "executable",
+			command = "codelldb",
+		}
+		dap.configurations.c = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					local target = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+					if vim.fn.filereadable("Makefile") == 1 then
+						vim.fn.system("make")
+					end
+					return target
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				runInTerminal = true,
+			},
+		}
 		dap.adapters["local-lua"] = {
 			type = "executable",
 			command = "node",
