7c79f4c02f339e15397566797519cb30045d95da
trigologiaa <augusto.ribero.15@gmail.com>
2026-01-16 18:23:28 -0300

    update: jdtls

--- lua/jdtls/jdtls_setup.lua
+++ lua/jdtls/jdtls_setup.lua
@@ -1,12 +1,16 @@
 local M = {}
 
-function M:setup()
+function M.setup()
+	local status_ok, blink = pcall(require, "blink.cmp")
+	local capabilities = status_ok and blink.get_lsp_capabilities() or vim.lsp.protocol.make_client_capabilities()
 	local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
 	local workspace_dir = "/home/gaston/development/jdtls_data/" .. project_name
+	local launcher_jar =
+		vim.fn.glob("/home/gaston/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar")
 	local config = {
 		cmd = {
 			"/usr/lib/jvm/java-25-openjdk/bin/java",
-			"-Declipse.application)org.eclipse.jdt.ls.core.id1",
+			"-Declipse.application=org.eclipse.jdt.ls.core.id1",
 			"-Dosgi.bundles.defaultStartLevel=4",
 			"-Dlog.protocol=true",
 			"-Dlog.level=ALL",
@@ -17,7 +21,7 @@ function M:setup()
 			"--add-opens",
 			"java.base/java.lang=ALL-UNNAMED",
 			"-jar",
-			"/home/gaston/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.7.100.v20251111-0406.jar",
+			launcher_jar,
 			"-configuration",
 			"/home/gaston/.local/share/nvim/mason/packages/jdtls/config_linux/",
 			"-data",
@@ -30,6 +34,7 @@ function M:setup()
 		init_options = {
 			bundles = {},
 		},
+		capabilities = capabilities,
 	}
 	require("jdtls").start_or_attach(config)
 end
